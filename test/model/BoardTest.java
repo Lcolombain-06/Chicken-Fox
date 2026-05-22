3f9e9f9045c596fa7271b82d8a73491b7430b665
Mahamane Mansourah <mansourah.mahamane@edu.univ-fcomte.fr>
2026-05-23 00:52:25 +0200

    Corrections erreur + ajout imports oublié

--- test/model/BoardTest.java
+++ test/model/BoardTest.java
@@ -1,23 +1,40 @@
 package model;
 
+import model.Board;
+import model.Cell;
+import model.Pawn;
+import boardifier.model.GameStageModel;
+import boardifier.model.StageElementsFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class BoardTest {
 
-    public class BoardTest {
-
-        private static class StubStageModel extends GameStageModel {
-            public StubStageModel() { super("test", null); }
-
-            @Override
-            public StageElementsFactory getDefaultElementFactory() { return null; }
+    private static class StubStageModel extends GameStageModel {
+        public StubStageModel() {
+            super("test", null);
         }
-        /** Le plateau que l'on teste, recréé avant chaque test. */
-        private Board board;
-        /**
-         * Méthode appelée avant chaque test.
-         * Elle crée un plateau vide
-         */
-        @BeforeEach
-        void setUp() {
-            board = new Board(0, 0, new StubStageModel());
+
+        @Override
+        public StageElementsFactory getDefaultElementFactory() {
+            return null;
         }
+    }
+
+    /**
+     * Le plateau que l'on teste, recréé avant chaque test.
+     */
+    private Board board;
+
+    /**
+     * Méthode appelée avant chaque test.
+     * Elle crée un plateau vide
+     */
+    @BeforeEach
+    void setUp() {
+        board = new Board(0, 0, new StubStageModel())
+    }
 }
