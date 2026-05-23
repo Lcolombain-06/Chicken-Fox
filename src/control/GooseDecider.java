ca64781d27ad8014fed3a106386aa0bdb33b3032
rachidahebie-cloud <rachidahebie@gmail.com>
2026-05-23 23:09:15 +0200

    verification des fonctions pour GooseDecider

--- src/control/GooseDecider.java
+++ src/control/GooseDecider.java
@@ -22,7 +22,7 @@ public class GooseDecider extends Decider {
         HoleStageModel stage = (HoleStageModel) model.getGameStage();
         Board board = stage.getBoard();
 
-        // 1. Locate the fox
+        // 1. Locate the fox 1
         int foxRow = -1, foxCol = -1;
         for (int y = 0; y < 7; y++) {
             for (int x = 0; x < 7; x++) {
