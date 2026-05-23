b35b73fb807bfd3c07255146dad0f37fb1e812cf
Mahamane Mansourah <mansourah.mahamane@edu.univ-fcomte.fr>
2026-05-23 16:23:11 +0200

    test

--- test/control/HoleControllerTest.java
+++ test/control/HoleControllerTest.java
@@ -37,7 +37,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class HoleControllerTest {
 
     // ---------------------------------------------------------------
-    // Infrastructure commune à tous les tests
+    // Infrastructure commune à tous les tests blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     // ---------------------------------------------------------------
 
     private Model          model;
